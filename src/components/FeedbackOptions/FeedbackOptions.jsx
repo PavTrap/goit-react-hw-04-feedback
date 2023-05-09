ff8645b477492c4e4a41e4700e1f2cdd44ff1c7d
Pavel_soundNotLight <105560877+PavTrap@users.noreply.github.com>
2023-05-09 17:05:30 +0300

    css feedbackoptions

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import css from './FeddbackOptions.module.css';
+import css from './FeddbackOptions.module.css';
 
 export const Feedback = ({ options, onLeaveFeedback }) => {
 	return (
